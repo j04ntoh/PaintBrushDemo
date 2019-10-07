a07c75e62f374d4db61bc0a2b606fd895cfe26b7
j04ntoh <j04n.toh@gmail.com>
2019-10-07 18:38:21 +0800

    Unity Files added

---
+++ Assets/PaintCraft/Engine/Scripts/Canvas/Configs/AdvancedPageConfig.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace PaintCraft.Canvas.Configs
+{
+    public abstract class AdvancedPageConfig : PageConfig {
+        public abstract Texture2D OutlineTexture{
+            get; 
+        }
+
+        public abstract Texture2D RegionTexture{
+            get;
+        }
+
+        public abstract Texture2D OverlayTexture
+        {
+            get;
+        }
+    }
+}
---
+++ Assets/PaintCraft/Engine/Scripts/Controllers/OutlineOverlayController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using PaintCraft.Utils;
+
+
+namespace PaintCraft.Controllers
+{
+    public class OutlineOverlayController : MonoBehaviour
+    {
+        CanvasController canvas;
+        private Material material;
+
+        public void Init(CanvasController canvas)
+        {
+            this.canvas = canvas;
+            UpdateMeshSize();
+        }
+
+        void UpdateMeshSize()
+        {
+            MeshFilter mf = GOUtil.CreateComponentIfNoExists<MeshFilter>(gameObject);
+            Mesh mesh = MeshUtil.CreatePlaneMesh(canvas.Width, canvas.Height);
+            mf.mesh = mesh;
+            MeshRenderer mr = GOUtil.CreateComponentIfNoExists<MeshRenderer>(gameObject);
+            material = new Material(Shader.Find("Unlit/Transparent"));
+            material.mainTexture = canvas.OverlayTexture;
+            mr.material = material;
+        }
+
+        public void SetNewSize()
+        {
+            MeshFilter mf = GOUtil.CreateComponentIfNoExists<MeshFilter>(gameObject);
+            MeshUtil.ChangeMeshSize(mf.mesh, canvas.Width, canvas.Height);
+            material.mainTexture = canvas.OverlayTexture;
+        }
+
+    }
+}
